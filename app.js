b081271b9c623647be1afcd58d32436e121557ab
Victoria Conlin <kinnevic@gmail.com>
2021-10-27 11:01:29 -0400

    Created app.js file. Also, created character generator functions.

---
+++ app.js
@@ -0,0 +1,43 @@
+// PASSWORD GENERATOR
+
+// Character Generator Function
+
+// Function that accepts a string value as an argument and returns a random index number from a string argument
+function randomIndex(str) {
+    return Math.floor(Math.random() * str.length);
+}
+// Example of the randomIndex function
+console.log(randomIndex(`example`)); // 0, 1, 2, 3, 4, 5, 6
+
+// Function that returns a random letter using a random index in the "letters" string
+function getRandomLower() {
+    const letters = `abcdefghijklmnopqrstuvwxyz`;
+    // Returning a random letter using a random index in the "letters" string
+    return letters[randomIndex(letters)];
+}
+console.log(getRandomLower());
+
+// Function that returns a random uppercase letter
+function getRandomUpper() {
+    // Running the "getRandomLower" function to create a random letter and setting that value to the letter variable
+    const letter = getRandomLower();
+    // Changing the random letter to an uppercase letter and returning it from the function
+    return letter.toUpperCase();
+}
+console.log(getRandomUpper());
+
+// Function that returns a random number as a string value
+function getRandomNumber() {
+    const numbers = `0123456789`;
+    // Returning a random number using a random index in the "numbers" string
+    return numbers[randomIndex(numbers)];
+}
+console.log(getRandomNumber());
+
+// Function that returns a random symbol as a string value 
+function getRandomSymbol() {
+    const symbols = `!@#$%^&*(){}[]=<>/,.`
+    // Returning a random symbols using a random index in the "symbols" string 
+    return symbols[randomIndex(symbols)];
+}
+console.log(getRandomSymbol());
